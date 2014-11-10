e4d47177d2615b3c1584c128da76a8063dfe15d3
Junya Nakazato <nakazato_junya@ca-adv.co.jp>
2014-11-10 19:39:33 +0900

    debug.php更新

--- debug.php
+++ debug.php
@@ -1,19 +1,25 @@
 <?php
-// はてなブックマークのWEBHOOKから、pinboardへブックマークするスクリプト
-// config読み込み
 $config_load = file_get_contents('setting.json');
 $config = json_decode($config_load);
 
-echo "config_load<hr />";
+echo "config_load<br />";
 var_dump($config_load);
+echo "<hr />";
 
-echo "config<hr />";
+echo "config<br />";
+// heroku側でAPIキーが設定されていれば、それらを優先
+if(isset($_SERVER['HATENA_WEBHOOK_KEY']) && $_SERVER['HATENA_WEBHOOK_KEY'] !== 'SET_UR_WEBHOOK_KEY'){
+  $config->hatena_webhook_key = $_SERVER['HATENA_WEBHOOK_KEY'];
+  $config->pinboard_token = $_SERVER['PINBOARD_TOKEN'];
+}
 var_dump($config);
+echo "<hr />";
 
-echo "_POST</hr />";
+echo "_POST</br />";
 var_dump($_POST);
+echo "<hr />";
 
-echo "_SERVER</hr />";
+echo "_SERVER</br />";
 var_dump($_SERVER);
+echo "<hr />";
 
-exit;
